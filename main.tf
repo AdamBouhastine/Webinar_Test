89f1a738fd6a941b6f47e86fedd33e6120526253
AdamBouhastine <bouhastine@et.esiea.fr>
2020-09-29 12:04:08 +0200

    Update Organization name

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "axdane"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Workspace_Webinar"
     }
   }
 }
